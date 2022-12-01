82e82a8c7d1fc0cc8a12514c9ce1a96a37848ab6
luisiglesiasgomez <luisiglesiasgomez@localhost>
2022-12-01 11:43:13 +0100

    Commit version inicial

---
+++ PruebaGIT/src/Start.java
@@ -0,0 +1,17 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hola Mundo GIT");
+		
+		
+		
+		
+		
+		
+	}
+
+}
